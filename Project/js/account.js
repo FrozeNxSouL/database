5e256bb2a2ae6e6c93885913622f250ed2d1cc65
Thitiphat <thitiphat6391@gmail.com>
2023-02-26 14:40:44 +0700

    remove comment

--- Project/js/account.js
+++ Project/js/account.js
@@ -2,20 +2,12 @@ const bgSignForm = document.querySelector('#sign-opt-bg');
 const signInForm = document.querySelector('#sign-in-form');
 const signUpForm = document.querySelector('#sign-up-form');
 const signForm = document.querySelector('#sign-form');
-// const summitlogin = document.querySelector('#summitlogin');
-// const summitsignin = document.querySelector('#summitsignin');
-// const userpic = document.querySelector('#material-symbols-outlined')
-// const check = document.getElementById("summit");
-// const phone = document.querySelector('#inputphonenum');
-// const warning = document.querySelector('#warning');
 
 bgSignForm.addEventListener('click', ()=> {
     signForm.style.display = 'none';
     signInForm.style.display = 'none';
     signUpForm.style.display = 'none';
     bgSignForm.style.display = 'none';
-    // userpic.style.display = 'inline-block';
-    // warning.style.display = 'none';
 })
 
 function signup() {
@@ -28,8 +20,3 @@ function signin() {
     bgSignForm.style.display = 'block';
     signUpForm.style.display = 'none';
 }
-
-// function checking() {
-    
-    
-// }
