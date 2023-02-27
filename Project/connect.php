674f6c07dd685896c99d186d87190b051c5f32ae
Thitiphat <thitiphat6391@gmail.com>
2023-02-27 15:30:03 +0700

    add branch page

--- Project/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = 'localhost';
-$user = 'root';
-$password = '123456789';
-$table = 'mc';
-
-$db = mysqli_connect($server,$user,$password,$table);
-?>
---
+++ Project/php/connect.php
@@ -0,0 +1,9 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '123456789';
+$dbname = 'mc';
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
