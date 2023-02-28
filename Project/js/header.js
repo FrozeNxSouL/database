700a22626c2bb7b35d669fdb5ee37b39d8cc70a9
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2023-02-28 19:15:59 +0700

    gay

--- Project/js/header.js
+++ Project/js/header.js
@@ -10,7 +10,7 @@ document.getElementById("header").innerHTML =
     "<div class='collapse navbar-collapse' id='navbarSupportedContent'>" +
     "<ul class='navbar-nav me-auto mb-2 mb-lg-0'>" +
     "<li class='nav-item'>" +
-    "<a class='nav-link' href='#'>Our Food</a>" +
+    "<a class='nav-link' href='OurFood.html'>Our Food</a>" +
     "</li>" +
     "<li class='nav-item'>" +
     "<a class='nav-link' href='#'>Store Location</a>" +
