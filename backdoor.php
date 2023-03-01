a8a0b5c24692719149c225a815cc75295761596a
Thitiphat <thitiphat6391@gmail.com>
2023-03-01 17:49:52 +0700

    prepare backdoor page

--- backdoor.php
+++ backdoor.php
@@ -1,51 +1,36 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="css/backdoor.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <header>
-            <div class="headbar">
-                <div class="logo">
-                    <a href="backdoor.php">
-                        <img src="img/logo.png" alt="logo">
-                    </a>
-                </div>
-                <div class="title">
-                    <h4>
-                        <span href="#" onclick="adjustmenu">MENU</span>
-                    </h4>
-                    <h4>
-                        <span href="#" onclick="adjustbranch">BRANCH</span>
-                    </h4>
-                    <h4>
-                        <span href="#" onclick="adjustuser">USER</span>
-                    </h4>
-                    <h4>
-                        <span href="#" onclick="adjustnews">NEWS</span>
-                    </h4>
-                    <h4>
-                        <span href="#" onclick="printfunc">PRINT</span>
-                    </h4>
-                </div>
-            </div>
-        </header>
-            </div>
-            <div class="main" >
-                <div class="adjustmenu" id="adjustmenu">
+<html lang="en">
+<head>
+    <title>MC</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="css/backdoor.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
 
-                </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</head>
 
-                <div class="adjustbranch" id="adjustbranch">
+<body>
+    <div class="content-header">
+        <ul class="nav-sub">
+            <li class="nav-subitem"><a class="nav-sublink" href="#">Slide show</a></li>
+            <li class="nav-subitem"><a class="nav-sublink" href="#">User</a></li>
+            <li class="nav-subitem"><a class="nav-sublink" href="#">Menu</a></li>
+            <li class="nav-subitem"><a class="nav-sublink" href="#">Delivery</a></li>
+        </ul>
+    </div>
+    <div class="contain">
+        <header id="header"></header>
+        <script src="js/header.js"></script>
+    </div>
 
-                </div>
-
-                <div class="adjustuser" id="adjustuser" >
-
-                </div>
-
-
-            </div>
-    </body>
+</body>
 </html>
