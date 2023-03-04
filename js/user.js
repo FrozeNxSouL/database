7ea311309be83c26f49b11bfede5bd2f39157ea7
Thitiphat <thitiphat6391@gmail.com>
2023-03-04 11:19:12 +0700

    user function

---
+++ js/user.js
@@ -0,0 +1,7 @@
+const info = document.getElementById('info');
+const transaction = document.getElementById('transaction');
+const receipt = document.getElementById('receipt');
+
+function user(page) {
+    
+}
