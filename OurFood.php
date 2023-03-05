5181e427f1db9553487d028efda83f41773ecd6d
Ratanon090 <Ratanon1@gmail.com>
2023-03-05 17:23:01 +0700

    cart.sql

--- OurFood.php
+++ OurFood.php
@@ -1,6 +1,7 @@
 <?php
 require('php/connect.php');
 
+
 $sql = 'SELECT * FROM food_menu WHERE food_category = 1;';
 $burgermenu = $conn->query($sql);
 
@@ -23,6 +24,8 @@ $menuset = $conn->query($sql2);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -44,7 +47,7 @@ $menuset = $conn->query($sql2);
             <li><a class="nav-sublink" href="#"  onclick="csub()">Other menu</a></li>
         </ul>
     </div>
-
+    <div id="message"></div>
     <div class="burgercontent" id="burger">
         <div class="menu-title">
             <h2 style="font-weight: 700">Menu</h2>
@@ -63,8 +66,15 @@ $menuset = $conn->query($sql2);
                         <h4 class="food-name"><?php echo $row["food_name"]; ?></h4>
                     </div>
                     <p class="pricehead" >Price</p>
-                    <h5>฿<?php echo $row["food_price"]; ?></h5>
-                    <button class="btn btn-danger">Order</button>
+                    <h5>฿<?php echo number_format($row["food_price"]) ?></h5>
+                    <form action="" class="form-submit">
+                        <input type="hidden" class="fid" value="<?php $row["food_id"] ?>">
+                        <input type="hidden" class="fname" value="<?php $row["food_name"] ?>">
+                        <input type="hidden" class="fprice" value="<?php $row["food_price"] ?>">
+                        <input type="hidden" class="fpict" value="<?php $row["food_pict"] ?>">
+                        <input type="hidden" class="fcategory" value="<?php $row["food_category"] ?>">
+                        <button class="btn btn-danger addItemBtn">Order</button>
+                    </form>
                 </div>
             <?php
                 }
@@ -90,7 +100,7 @@ $menuset = $conn->query($sql2);
                         <h4 class="food-name"><?php echo $row["menuset_name"]; ?></h4>
                     </div>
                     <p class="pricehead" >Price</p>
-                    <h5>฿<?php echo $row["menuset_price"]; ?></h5>
+                    <h5>฿<?php echo number_format($row["menuset_price"]) ?></h5>
                     <button class="btn btn-danger">Order</button>
                 </div>
             <?php
@@ -117,7 +127,7 @@ $menuset = $conn->query($sql2);
                         <h4 class="food-name"><?php echo $row["food_name"]; ?></h4>
                     </div>
                     <p class="pricehead" >Price</p>
-                    <h5>฿<?php echo $row["food_price"]; ?></h5>
+                    <h5>฿<?php echo number_format($row["food_price"]) ?></h5>
                     <button class="btn btn-danger">Order</button>
                 </div>
             <?php
@@ -129,6 +139,28 @@ $menuset = $conn->query($sql2);
 
     <script src="js/foodselector.js"></script>
     <?php require 'php/module/footer.html'?>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(".btn btn-danger addItemBtn").click(function(e){
+                e.preventDefault();
+                var $form = $(this).closest(".form-submit");
+                var fid = $form.find(".fid").val();
+                var fname = $form.find(".fname").val();
+                var fprice = $form.find(".fprice").val();
+                var fpict = $form.find(".fpict").val();
+                var fcategory = $form.find(".fcategory").val();
+
+                $.ajax({
+                    url: "action.php",
+                    method: "post",
+                    data: {fid:fid,fname:fname,fprice:fprice,fpict:fpict,fcategory:fcategory},
+                    success:function(response){
+                        $("message").html(response);
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 </html>
 
