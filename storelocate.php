de6d3050f446b2a0969a95c9d51ee0206fea85bf
Thitiphat <thitiphat6391@gmail.com>
2023-03-06 20:14:10 +0700

    styling store locate

--- storelocate.php
+++ storelocate.php
@@ -29,55 +29,53 @@
         </header>
     </div>
     <div class="content-header">
-        <div class="outerbox" >
-            <div class="itc">
-                <form class="input-container" method="POST">
-                    <select name="select-province" id="select-province" class="select-province">
-                        <?php
-                            $provincesql = "SELECT name_th FROM provinces";
-                            $province = $conn->query($provincesql);
-                            while ($poption = mysqli_fetch_assoc($province))  {
-                        ?>
-                            <option value="<?php echo $poption['name_th']; ?>"><?php echo $poption['name_th']; ?></option>
-                        <?php } ?>
-                    </select>
-                    <!-- <div class="input-border">
+        <div class="head-input">
+            <form class="input-container" method="POST">
+                
+                <select name="select-province" id="select-province" class="select-province">
+                    <?php
+                        $provincesql = "SELECT name_th FROM provinces";
+                        $province = $conn->query($provincesql);
+                        while ($poption = mysqli_fetch_assoc($province))  {
+                    ?>
+                        <option value="<?php echo $poption['name_th']; ?>"><?php echo $poption['name_th']; ?></option>
+                    <?php } ?>
+                </select>
 
-                    </div> -->
-
-                    <input type="text" class="searchbar" id="searchstore" name="search" placeholder="Search">
-                    <button type="submit" class="searchbtn" name="submit">Search</button>
-                </form>
-            </div>
+                <input type="text" class="searchbar" id="searchstore" name="search" placeholder="Search">
+                <button type="submit" class="searchbtn" name="submit">Search</button>
+            </form>
         </div>
     </div>
-    <div class="locatetype" >
-        <a href="#" id="listview" class="listview" onclick="cdlist()">LIST VIEW</a>
-        <a href="#" id="mapview" class="mapview" onclick="cdmap()">MAP VIEW</a>
-    </div>
-    <div id="listcontent" class="listcontent">
-        <?php 
-            if(isset($_POST['submit'])) {
-                $slpro = $_POST['select-province'];
-                $search = $_POST['search'];
-                if ($search == '') {
-                    $storesql = "SELECT * FROM branch WHERE branch_province = '$slpro'";
+    <div class="content-main">
+        <div class="locatetype" >
+            <a href="#" id="listview" class="listview" onclick="cdlist()">LIST VIEW</a>
+            <a href="#" id="mapview" class="mapview" onclick="cdmap()">MAP VIEW</a>
+        </div>
+        <div id="listcontent" class="listcontent">
+            <?php 
+                if(isset($_POST['submit'])) {
+                    $slpro = $_POST['select-province'];
+                    $search = $_POST['search'];
+                    if ($search == '') {
+                        $storesql = "SELECT * FROM branch WHERE branch_province = '$slpro'";
+                    }
+                    else {
+                        $storesql = "SELECT * FROM branch WHERE branch_province = '$slpro' AND (branchName = '$search' OR branch_subdistrict = '$search' OR branch_district = '$search') ";
+                    }    
                 }
-                else {
-                    $storesql = "SELECT * FROM branch WHERE branch_province = '$slpro' AND (branchName = '$search' OR branch_subdistrict = '$search' OR branch_district = '$search') ";
-                }    
-            }
-            $qr = $conn->query($storesql);
-            while ($store = mysqli_fetch_assoc($qr)) {
-        ?>
-        <div class="itemcard">
-            <h6><?php echo $store['branchName'] ?></h6>
-            <p><?php echo $store['branch_address'];?> <?php echo $store['branch_subdistrict'];?>,<?php echo $store['branch_district'];?>,<?php echo $store['branch_province']; ?></p>
+                $qr = $conn->query($storesql);
+                while ($store = mysqli_fetch_assoc($qr)) {
+            ?>
+            <div class="itemcard">
+                <h6><?php echo $store['branchName'] ?></h6>
+                <p><?php echo $store['branch_address'];?> <?php echo $store['branch_subdistrict'];?>,<?php echo $store['branch_district'];?>,<?php echo $store['branch_province']; ?></p>
+            </div>
+            <?php } ?>
         </div>
-        <?php } ?>
-    </div>
-    <div id="mapcontent" class="mapcontent">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.3312523723316!2d100.51073740510338!3d13.819137000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29b877800c9af%3A0xd754c571fc7177b!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LmC4LiZ4LmC4Lil4Lii4Li14Lie4Lij4Liw4LiI4Lit4Lih4LmA4LiB4Lil4LmJ4Liy4Lie4Lij4Liw4LiZ4LiE4Lij4LmA4Lir4LiZ4Li34Lit!5e0!3m2!1sth!2sth!4v1677977869587!5m2!1sth!2sth" width="1200" height="760" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div id="mapcontent" class="mapcontent">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.3312523723316!2d100.51073740510338!3d13.819137000000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29b877800c9af%3A0xd754c571fc7177b!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LmC4LiZ4LmC4Lil4Lii4Li14Lie4Lij4Liw4LiI4Lit4Lih4LmA4LiB4Lil4LmJ4Liy4Lie4Lij4Liw4LiZ4LiE4Lij4LmA4Lir4LiZ4Li34Lit!5e0!3m2!1sth!2sth!4v1677977869587!5m2!1sth!2sth" width="1200" height="760" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
     </div>
     <script src="js/storelocate.js"></script>
     <?php include 'php/module/footer.html' ?>
